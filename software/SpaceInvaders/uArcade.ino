4591b1cf5550933d81ebcc64fed929f787e32df0
Christophe Cachin <C.Cachin@ch.ccv.eu>
2017-08-25 15:47:48 +0200

    Add tone on buzzer
    And reset when game is done

--- software/SpaceInvaders/uArcade.ino
+++ software/SpaceInvaders/uArcade.ino
@@ -14,10 +14,13 @@
 //    b. keep the original credits.
 
 #include <U8glib.h>
+#include "pitches.h"
 
 static const uint32_t splash [] U8G_PROGMEM = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7936,0,0,0,536362880,520095680,127,25171968,0x7FF8FFE0,0x3FC007C0,127,25171968,0x7FF87FE0,0x3FE00FE0,127,6316032,0xF8F871F0,0x3FF00FE0,7,6316032,0xF8F831F0,838344672,7,33552384,0xF0F811F0,7872480,7,33552384,0xF0F801F0,3677936,7,0x79F9E00,0xF0F807F0,3940080,119,0x79F9E00,0xF8F81FF0,3947640,127,536870784,0xFCF83FF0,3947640,127,536870784,0x7FF8FFE0,3947640,127,436205952,0x7FF9FF80,3963000,7,436205952,536475648,7895164,7,427825536,0xF9F000,821592060,7,427825536,0xF9F000,0x3FF87FFC,7,7987200,0xF9F000,0x3FF0FFFC,127,7987200,0xF9F1E0,0x3FE0F03E,127,0,0xF8F9F0,0x3FC0F03E,127,0,0xF8FFF0,520220734,1,0,0xF87FF0,4159,3145728,0,0xF03FC0,0,8128496,0,384,0xF8080000,8265715,0,0,0xFC2FF004,8265715,0,0xC1F00000,0xF83FF00F,2571507,0,0xC0F007CC,947908623,474224,0x801E0000,0xC0F807CF,947712031,473200,0x801F07C0,0xE0FC0F8F,955281439,2047088,0x807F0FC0,0xE0FC1F8F,955314239,8339056,0x80FF0FC0,0xE0781F8F,0xB9E0F03F,0xFE1FF3,0x81FF0FC0,0xF07C1F0F,0xF9E0F03D,0xFC0FF3,0x81FF0FC0,0xF07E3F0F,0xF9E0F03D,0xE00FF3,0x81FF0FC0,0xF03E3F0F,0xF9E0F078,0xE007F3,0x7FF0FC0,0xF83E3E0F,971042936,0xE20F70,0xFFF0FC0,0xF83E3E0F,954265720,0xF60F70,536809408,0x781F7E1F,955314416,8330864,0x3F9F0FC0,0x7C1F7C1F,955314416,4136048,0x3F9F0FC0,0xFC1FF81F,0xF878F0FF,3947635,0x3F1F0FC0,0xFC0FF81F,0xF87CF1FF,14451,0xFE1F0FC0,0xFC0FF01F,0xF83FF1FF,51,0xFC1F0FC0,0xFE0FF01F,941617632,0,0xF81F0FC0,0x3E0FF01F,0xFF1E0,0,0xF01F0FC0,0x3E07F01F,5088,0,0xF01F0FC0,0x3E07E01F,0,0,0xE03F0FC0,520347679,0,0,0xC03F0FC0,245791,16777216,524288,0x803F0FC0,31,16777216,786432,4132800,15,41943040,524288,4132800,0,0x82866000,25889906,462784,0,0xC4444000,38572617,1984,0,0x87C44000,63478792,0,0,0x88244000,4755976,0,0,0x882E4000,39621192,0,0,0xDC758000,25902129,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 static const uint16_t monsters[] U8G_PROGMEM = {0x0600,0x0F00,0x1F80,0x36C0,0x3FC0,0x0900,0x1680,0x2940,0x0600,0x0F00,0x1F80,0x36C0,0x3FC0,0x0900,0x1080,0x0900,0x2080,0x9120,0xBFA0,0xEEE0,0x7FC0,0x3F80,0x2080,0x4040,0x2080,0x1100,0x3F80,0x6EC0,0xFFE0,0xBFA0,0xA0A0,0x1B00,0x0E00,0x7FC0,0xFFE0,0xCE60,0xFFE0,0x3F80,0x6EC0,0xC060,0x0E00,0x7FC0,0xFFE0,0xCE60,0xFFE0,0x3F80,0x6EC0,0x3180};
-  
+
+const int BUZZER_PIN =11;
+
 //U8GLIB_SSD1306_128X64 u8g(13, 11, 10, 9);  // SW SPI Com: SCK = 13, MOSI = 11, CS = 10, d/c A0 = 9
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE); // I2C: SCL=A5,SDA=A4
 
@@ -25,7 +28,7 @@ const int screenW = 128, screenH = 64;
 enum GameState { GAME_NORMAL, GAME_WIN, GAME_OVER } g_gameState;
 
 // encoder debounce was taken from: http://playground.arduino.cc/Main/RotaryEncoders
-enum EncoderPins {  ENCODER_PIN_RIGHT = 2, ENCODER_PIN_LEFT = 3, ENCODER_PIN_FIRE = 12};
+enum EncoderPins {  ENCODER_PIN_LEFT = 2, ENCODER_PIN_RIGHT = 3, ENCODER_PIN_FIRE = 12};
 
 volatile unsigned char encoderPos = 127;  // a counter for the dial
 unsigned int lastReportedPos = 1;         // change management
@@ -54,6 +57,8 @@ bool checkBtn() {
   if ((millis() - lastTime) > debounce) {
     if (reading != state) {
       state = reading;
+      tone(BUZZER_PIN,2093,50);
+      delay(20);
       if (state == LOW) {
         return true;
       }
@@ -62,6 +67,19 @@ bool checkBtn() {
   lastPress = reading;
   return false;
 }
+
+void(* resetFunc) (void) = 0;
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
 
 void doEncoderA() { // Interrupt on A changing state
   // debounce
@@ -237,7 +255,6 @@ public:
   }
   void hitTest() {
     bool hit = false;
-
     for (int i = 0; i < Lasers::MAX_LASERS; i++) {
       // quick fallbacks
       uint8_t y = lasers._y[i];
@@ -260,6 +277,7 @@ public:
       _data[col + row * MAX_COLS] = BLANK_IDX;
       lasers._y[i] = 0xff;
       hit = true;
+      tone(BUZZER_PIN,523,50);
     }
 
     if (!hit) {
@@ -342,7 +360,23 @@ void loop(void) {
       if (g_gameState == GAME_OVER) {
         u8g.drawStr(26, 33, "Game Over");
       } else {
-        u8g.drawStr(26, 33, "Press RST");
+        u8g.drawStr(26, 33, "YOU ROCK!!!");
+
+          for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+          // to calculate the note duration, take one second divided by the note type.
+          //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+          int noteDuration = 1000 / noteDurations[thisNote];
+          tone(11, melody[thisNote], noteDuration);
+      
+          // to distinguish the notes, set a minimum time between them.
+          // the note's duration + 30% seems to work well:
+          int pauseBetweenNotes = noteDuration * 1.30;
+          delay(pauseBetweenNotes);
+          // stop the tone playing:
+          noTone(8);
+        }   
+        resetFunc();
       }
     } while (u8g.nextPage());
   }
